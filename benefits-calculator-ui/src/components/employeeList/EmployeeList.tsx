606c3b852e7eb56af0f79cb8d1b5df6770965357
Benjamin Grebner <bgrebner@gmail.com>
2021-02-27 13:02:43 -0500

    add button.

--- benefits-calculator-ui/src/components/employeeList/EmployeeList.tsx
+++ benefits-calculator-ui/src/components/employeeList/EmployeeList.tsx
@@ -1,8 +1,13 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import "./employeeList.css";
 
 const EmployeeList = () => (
-    <h1 className="centered">Employees</h1>
+    <div id="employeeList">
+        <h1 className="centered">Employees</h1>
+
+        <Button className="float-right" variant="contained" color="primary">Add Employee</Button>
+    </div>
 );
 
 export default EmployeeList;
