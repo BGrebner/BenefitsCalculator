53263f319c762beebd8f14284ce6f284027b7c50
Benjamin Grebner <bgrebner@gmail.com>
2021-02-27 12:33:12 -0500

    add styling to employee  list component

--- benefits-calculator-ui/src/components/employeeList/EmployeeList.tsx
+++ benefits-calculator-ui/src/components/employeeList/EmployeeList.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./employeeList.css";
 
 const EmployeeList = () => (
-    <h1>Employee List</h1>
+    <h1 className="centered">Employees</h1>
 );
 
 export default EmployeeList;
