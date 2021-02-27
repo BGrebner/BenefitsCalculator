45e3008a2ee5cbbff4e247aff771c66f52ca40f8
Benjamin Grebner <bgrebner@gmail.com>
2021-02-27 17:56:18 -0500

    create add dependent button

--- benefits-calculator-ui/src/components/employee/EmployeeForm.tsx
+++ benefits-calculator-ui/src/components/employee/EmployeeForm.tsx
@@ -1,4 +1,4 @@
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import React from "react";
 import "./employeeForm.css";
 
@@ -7,6 +7,8 @@ const EmployeeForm = () => (
         <h2>New Employee</h2>
         <TextField label="First Name" />
         <TextField label="Last Name" />
+        <br/>
+        <Button className="float-right" variant="contained" color="primary">Add Dependent</Button>
     </form>
 );
 
