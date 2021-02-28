ed3ce32bd9bd76a71dc258873cf7badebd8dede2
Benjamin Grebner <bgrebner@gmail.com>
2021-02-28 11:44:16 -0500

    test  updating employee state

--- benefits-calculator-ui/src/components/employee/EmployeeForm.test.tsx
+++ benefits-calculator-ui/src/components/employee/EmployeeForm.test.tsx
@@ -1,10 +1,9 @@
 import React from "react"; 
-import {cleanup, render} from "@testing-library/react"; 
+import {cleanup, fireEvent, getByDisplayValue, render} from "@testing-library/react"; 
 import EmployeeForm from "./EmployeeForm";
 import Employee from "../../models/Employee";
 import Person from "../../models/Person";
 
- 
 afterEach(cleanup);
 
 function renderEmployeeForm(args?: any) { 
@@ -16,22 +15,15 @@ function renderEmployeeForm(args?: any) {
     return render(<EmployeeForm {...props} />); 
 }
 
-it('contains add dependent button', () => {
+it('should contain add dependent button', () => {
     const { getByText } = renderEmployeeForm();
 
     getByText("Add Dependent");
 });
 
-it('maintains state', () => {
-    const dependents: Array<Person> = [
-        {firstName: "Luke", lastName: "Skywalker"},
-        {firstName: "Leia", lastName: "Organa"}
-    ];
-    const currentState: Employee = {
-        firstName: "Darth",
-        lastName: "Vader",
-        dependents: dependents
-    };
+it('should maintain state', () => {
+    const currentState: Employee = createInitialState();
+    const dependents: Array<Person> = currentState.dependents || [];
 
     const { getByDisplayValue } = renderEmployeeForm({employee: currentState});
 
@@ -42,3 +34,40 @@ it('maintains state', () => {
     getByDisplayValue(dependents[1].firstName);
     getByDisplayValue(dependents[1].lastName);
 });
+
+describe('updating employee state', () => {
+    it('should update employee first name', () => {
+        const { getByLabelText } = renderEmployeeForm();
+        const expectedFirstName = "Anakin";
+
+        const input = getByLabelText("First Name") as HTMLInputElement;
+
+        fireEvent.change(input, { target: { value: expectedFirstName}});
+
+        expect(input.value).toBe(expectedFirstName);
+    });
+
+    it('should update employee last name', () => {
+        const { getByLabelText } = renderEmployeeForm();
+        const expectedLastName = "Skywalker";
+
+        const input = getByLabelText("Last Name") as HTMLInputElement;
+
+        fireEvent.change(input, { target: { value: expectedLastName}});
+
+        expect(input.value).toBe(expectedLastName);
+    });
+})
+
+const createInitialState: () => Employee = () => {
+    const dependents: Array<Person> = [
+        {firstName: "Luke", lastName: "Skywalker"},
+        {firstName: "Leia", lastName: "Organa"}
+    ];
+    const currentState: Employee = {
+        firstName: "Darth",
+        lastName: "Vader",
+        dependents: dependents
+    };
+    return currentState;
+}
