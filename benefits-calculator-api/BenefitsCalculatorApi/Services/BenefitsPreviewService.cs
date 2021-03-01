140dd6c1e13c09d88ef3f4d78dfcac0f601e3f60
Benjamin Grebner <bgrebner@gmail.com>
2021-03-01 18:50:46 -0500

    create and apply rule to employee.

--- benefits-calculator-api/BenefitsCalculatorApi/Services/BenefitsPreviewService.cs
+++ benefits-calculator-api/BenefitsCalculatorApi/Services/BenefitsPreviewService.cs
@@ -10,9 +10,17 @@ namespace BenefitsCalculatorApi.Services
 
     public class BenefitsPreviewService : IBenefitsPreviewService
     {
+        private const decimal STANDARD_EMPLOYEE_COST = 1000;
+
         public decimal CalculateBenefitsCost(Employee employee)
         {
-            return 1000;
+            return ApplyNameRule(employee.FirstName, employee.LastName, STANDARD_EMPLOYEE_COST);
         }
+
+        private decimal ApplyNameRule(string firstName, string lastName, decimal benefitCost) => benefitCost * (NameDiscountApplies(firstName, lastName) ? (decimal).9 : 1);
+
+        private bool NameDiscountApplies(string firstName, string lastName) => firstName.StartsWith("a", StringComparison.InvariantCultureIgnoreCase) 
+            || lastName.StartsWith("a", StringComparison.InvariantCultureIgnoreCase);
+
     }
 }
